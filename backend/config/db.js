c02f91d1f7ce66a5edc04e1d9d93c6b74211265f
Maleesa <131164297+Maleesanat01@users.noreply.github.com>
2025-02-06 09:34:01 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
 dotenv.config();
 
@@ -9,7 +9,7 @@ const connectDB = async () => {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
-        
+
         console.log('MongoDB connected');
 
         // Event listeners
@@ -41,22 +41,3 @@ const connectDB = async () => {
 };
 
 export default connectDB;
-
-
-
-// const mongoose = require('mongoose');
-
-// const connectDB = async () => {
-//   try {
-//     await mongoose.connect(process.env.MONGO_URI, {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//     });
-//     console.log('MongoDB connected');
-//   } catch (error) {
-//     console.error('MongoDB connection failed', error);
-//     process.exit(1);
-//   }
-// };
-
-// module.exports = connectDB;
